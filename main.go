3dc4c67ba54ecc362a45de96b950dcff5361d044
SaDeGh <stabasi76@gmail.com>
2022-08-10 15:00:10 +0430

    comment namespace controller

--- main.go
+++ main.go
@@ -89,13 +89,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ArgocdUser")
 		os.Exit(1)
 	}
-	if err = (&controllers.NamespaceReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
-		os.Exit(1)
-	}
+	// if err = (&controllers.NamespaceReconciler{
+	// 	Client: mgr.GetClient(),
+	// 	Scheme: mgr.GetScheme(),
+	// }).SetupWithManager(mgr); err != nil {
+	// 	setupLog.Error(err, "unable to create controller", "controller", "Namespace")
+	// 	os.Exit(1)
+	// }
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
