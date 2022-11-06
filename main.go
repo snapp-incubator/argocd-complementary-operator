b57d2c9b369970acddf842141ff9ae5fbfa35ed6
nima-rx <nima.riahi@gmail.com>
2022-11-06 13:05:09 +0330

    uncomment ns controller

--- main.go
+++ main.go
@@ -91,13 +91,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	// if err = (&controllers.NamespaceReconciler{
-	// 	Client: mgr.GetClient(),
-	// 	Scheme: mgr.GetScheme(),
-	// }).SetupWithManager(mgr); err != nil {
-	// 	setupLog.Error(err, "unable to create controller", "controller", "Namespace")
-	// 	os.Exit(1)
-	// }
+	if err = (&controllers.NamespaceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
+		os.Exit(1)
+	}
 
 	//+kubebuilder:scaffold:builder
 
