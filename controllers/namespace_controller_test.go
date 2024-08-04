1b149b74bd56687bfd4e75046733da45ea0c713f
Parham Alvani <parham.alvani@gmail.com>
2024-08-04 16:32:20 +0000

    chore: use empty over destinations

--- controllers/namespace_controller_test.go
+++ controllers/namespace_controller_test.go
@@ -124,7 +124,7 @@ var _ = Describe("namespace controller", func() {
 				}, timeout, interval).Should(BeTrue())
 				Expect(testAppProj.Name).Should(Equal(appProjLookupKey.Name))
 				Expect(testAppProj.Namespace).Should(Equal(appProjLookupKey.Namespace))
-				Eventually(testAppProj.Spec.Destinations, timeout, interval).Should(BeNil())
+				Eventually(testAppProj.Spec.Destinations, timeout, interval).Should(BeEmpty())
 			})
 		})
 	})
